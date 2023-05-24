ec96368c4b047e8ef000dc16725998e5d57d7d55
Bright Uzosike <michizler.brandforyou@gmail.com>
2023-05-25 00:02:46 +0100

    First app deployment

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("node:https");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    var firstName = req.body.firstname;
+    var lastName = req.body.lastname;
+    var email = req.body.email;
+
+    const data = {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+        },
+    };
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us8.api.mailchimp.com/3.0/lists/6194b3c41d/members";
+    const options = {
+        method: "POST",
+        auth: "michizler:f27e81afbb87ef57374e04b21467585a-us8"
+    };
+
+    const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+       response.on("data", function(data){
+        console.log(JSON.parse(data));
+       });
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT  || 3000, function(){
+    console.log("Server running on port 3000");
+});
+
+//API_KEY
+// f27e81afbb87ef57374e04b21467585a-us8
+
+//LIST_ID
+// 6194b3c41d
